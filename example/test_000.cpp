b8901c75db8068f8562776e0eb5951dd13b03f1a
Shogo Sawai <shogo.sawai@gmail.com>
2018-11-22 12:59:37 +0900

    add default main

--- example/test_000.cpp
+++ example/test_000.cpp
@@ -25,32 +25,34 @@ class MyFactory : public Factory {
         z[1] = cos(w[0] * 0.5);
         z[2] = exp(w[0] * x[1]);
 
-        std::cout << y[0] << ", " << y[1] << std::endl;
+        // std::cout << y[0] << ", " << y[1] << std::endl;
 
         auto f = functions[0]->shared_from_this();
     }
 
 };
 
-int main(int argc, char **argv) {
-    MyFactory f;
-    f.generate();
-    std::cout << f.digraph() << std::endl;
-    std::cout << f.wholeGraph() << std::endl;
-    std::cout << f.cxxCodePrinter("namespace_name", "ClassName") << std::endl;
+// int main(int argc, char **argv) {
+//     MyFactory f;
+//     f.generate();
+//     std::cout << f.digraph() << std::endl;
+//     std::cout << f.wholeGraph() << std::endl;
+//     std::cout << f.cxxCodePrinter("namespace_name", "ClassName") << std::endl;
 
-    auto x = _make_shared<sym::Variable>("x");
-    auto y = _make_shared<sym::Variable>("y");
+//     auto x = _make_shared<sym::Variable>("x");
+//     auto y = _make_shared<sym::Variable>("y");
     
-    auto z = x + y;
-    auto dz = z->diff(x);
-    dz->simplified();
-    std::cout << "x = " << x << std::endl;
-    std::cout << "y = " << y << std::endl;
-    std::cout << "z = " << z << std::endl;
-    std::cout << "dz/dx = " << dz << " : " << "is_one=" << is_one(dz) << ", is_constant=" << is_constant(dz) << std::endl;
+//     auto z = x + y;
+//     auto dz = z->diff(x);
+//     dz->simplified();
+//     std::cout << "x = " << x << std::endl;
+//     std::cout << "y = " << y << std::endl;
+//     std::cout << "z = " << z << std::endl;
+//     std::cout << "dz/dx = " << dz << " : " << "is_one=" << is_one(dz) << ", is_constant=" << is_constant(dz) << std::endl;
 
-    sym::Repr r = sym::_repr("(", 3, "+", 4, ")");
+//     sym::Repr r = sym::_repr("(", 3, "+", 4, ")");
     
-    return 0;
-}
+//     return 0;
+// }
+
+OUTPUT_CXX_CODE_MAIN(MyFactory);
--- include/sym/sym.hpp
+++ include/sym/sym.hpp
@@ -19,12 +19,54 @@
 #include "unary_function_impl.hpp"
 #include "binary_function_impl.hpp"
 
+#include <iostream>
 
 namespace sym {
 
 template<class F>
-int default_main(int arg, char **argv) {
+int default_main(int argc, char **argv) {
     F factory;
+
+    bool test_mode = false, dag_mode = false;
+    std::string function_name, ns_name, class_name;
+    for (int arg = 1; arg < argc; arg++) {
+        auto check_arg = [&](char short_opt, const std::string &long_opt, bool increment_arg) {
+            if (std::string(argv[arg]) == std::string("-") + std::string({short_opt}) or
+                std::string(argv[arg]) == std::string("--") + long_opt) {
+                if (increment_arg) {
+                    arg++;
+                }
+                return true;
+            }
+            return false;
+        };
+        if (check_arg('f', "function-name", true)) {
+            function_name = argv[arg];
+        } else if (check_arg('n', "namespace", true)) {
+            ns_name = argv[arg];
+        } else if (check_arg('c', "class", true)) {
+            class_name = argv[arg];
+        } else if (check_arg('\0', "test", false)) {
+            test_mode = true;
+        } else if (check_arg('\0', "dag", false)) {
+            dag_mode = true;
+        }
+    }
+
+    factory.generate();
+
+    if (test_mode) {
+        
+    } else if (dag_mode) {
+        std::cout << factory.digraph() << std::endl;
+    } else if (class_name != "") {
+        std::cout << factory.cxxCodePrinter(ns_name, class_name) << std::endl;
+    } else if (function_name != "") {
+        
+    } else {
+        std::cerr << "no option specified" << std::endl;
+        return -1;
+    }
     return 0;
 }
 
