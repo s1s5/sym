1fd0d73c1f6215ffd9af58e8e01ee6824ee2cace
Shogo Sawai <shogo.sawai@gmail.com>
2018-12-02 21:22:40 +0900

    add md extractor

---
+++ include/sym/md_extractor.hpp
@@ -0,0 +1,25 @@
+/**
+ * Copyright Shogo Sawai
+ * @file md_extractor.hpp
+ * @brief
+ * @author Shogo Sawai
+ * @date 2018-12-01 21:59:24
+ */
+#ifndef MD_EXTRACTOR_HPP_
+#define MD_EXTRACTOR_HPP_
+
+#include "function.hpp"
+#include "unary_function.hpp"
+#include "binary_function.hpp"
+
+namespace sym {
+
+class MDExtractor {
+ public:
+
+};
+
+}  // namespace sym
+
+
+#endif  // MD_EXTRACTOR_HPP_
